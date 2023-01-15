20dd980b647a8ee6860ab004cb67076ae8ce3366
mexican1233455 <josehernandez122344@gmail.comgit config --global user.email>
2023-01-14 17:22:09 -0700

    prove assignment

---
+++ proveassignment2.py
@@ -0,0 +1,20 @@
+# Grap questions for user 
+adjective= input('adjective:') 
+person= input("person")
+verb3= input("verb")
+exclamation=input("exclamation").capitalize()
+verb1= input("verb")
+verb2= input("verb")
+animal=input("animal")
+# print out/story
+
+print('Your story is')
+
+print(f'''
+The other day, I was really in trouble. It all started when I saw a very
+{adjective} {animal} {verb1} down the hallway. "{exclamation}!" I yelled. But all
+I could think to do was to {verb2} over and over. Miraculously,
+that caused it to stop, but not before it tried to {verb3}
+right in front of my family.
+''')
+
